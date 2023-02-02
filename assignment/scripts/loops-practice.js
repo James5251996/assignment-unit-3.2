be0020ffdfaeb6e0a7cbb93a4522611a35ad0e16
Trent Osterman <trentosterman@gmail.com>
2023-02-02 14:56:57 -0600

    Got the computer to log the individual stars in a loop.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -36,11 +36,13 @@ for (let i=5; i>-1; i--) {
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
-let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
+let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb'];
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for (star of stars) {
+  console.log('this is the ', star);
+};
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
